c31ff6f56a93c0e8aa115f70b31836aca48bf978
cinfacol <cinfacol@gmail.com>
2024-03-30 04:55:50 -0500

    configurada la parte inicial del proyecto

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Sidebar from "./components/Sidebar";
+import BoardTasks from "./components/BoardTasks";
+
+export default function Home() {
+  return (
+    <main className="flex h-full">
+      <Sidebar />
+      <BoardTasks />
+    </main>
+  );
+}
